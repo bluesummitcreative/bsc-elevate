18d7e551ec9aa7d907255cce4d95e89561564449
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-17 13:38:18 -0600

    Update slider-home.php

--- inc/slider-home.php
+++ inc/slider-home.php
@@ -1,5 +1,29 @@
-<?php if( get_field('slider') ): ?>
 <div class="slider">
-	 <?php echo do_shortcode(get_field('slider', false, false)); ?>
+	 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="http://via.placeholder.com/1920x800" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://via.placeholder.com/1920x800" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://via.placeholder.com/1920x800" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </div>
 <?php endif; ?>
