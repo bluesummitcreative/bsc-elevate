524b51dfe99afb02326d1873f94f927911b99bea
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-17 13:58:31 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 	require_once('functions/enqueue-styles-scripts.php');
-	require_once('functions/customizer.php');
+	//require_once('functions/customizer.php');
 	require_once('functions/custom-walker.php');
 	require_once('functions/gravity-forms.php');
 	require_once('functions/register-menus.php');	
