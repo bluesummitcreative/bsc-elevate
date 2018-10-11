c3238f24298dcdc4a613ad7475a4d4b15d212219
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-11 06:55:45 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -2,7 +2,6 @@
 	/*Template Name: Home*/
 	get_header();
 	echo do_shortcode('[rev_slider alias="home_slider"]');
-	get_template_part('inc/intro-home');
 	get_template_part('inc/funnels-home');
 	get_template_part('inc/site-wide-cta');
 	get_footer(); 
