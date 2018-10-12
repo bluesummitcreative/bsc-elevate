a1a8b3ed088c5a559f6951174e58bb3f3188eaf0
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-12 15:45:53 -0600

    Update shortcodes.php

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -2,7 +2,7 @@
 	// Admin Email
 	function bsc_admin_email_shortcode() {
 		$bsc_admin_email = get_option('admin_email');
-		return $bsc_admin_email;
+		return '<a href="mailto:'.$bsc_admin_email.'">'.$bsc_admin_email.'</a>';
 	}
 	add_shortcode( 'admin-email', 'bsc_admin_email_shortcode' );
 ?>
