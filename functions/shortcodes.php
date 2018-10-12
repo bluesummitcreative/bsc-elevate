859d9967db199adcc94451834750ee2973d1cbd9
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-12 15:49:02 -0600

    Update shortcodes.php

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -5,4 +5,11 @@
 		return '<a href="mailto:'.$bsc_admin_email.'">'.$bsc_admin_email.'</a>';
 	}
 	add_shortcode( 'admin-email', 'bsc_admin_email_shortcode' );
+
+	// Site URL
+	function bsc_siteurl_shortcode() {
+		$bsc_siteurl = get_option('siteurl');
+		return '<a href="'.$bsc_siteurl.'">'.$bsc_siteurl.'</a>';
+	}
+	add_shortcode( 'siteurl', 'bsc_siteurl_shortcode' );
 ?>
