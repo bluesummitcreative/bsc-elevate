c79ea36837d67e78e7ae3cc0f7bcd0ba6500733e
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-11-03 16:17:37 -0600

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,8 @@
+<?php 
+	/*Template Name: Home*/
+	get_header();
+	echo do_shortcode('[rev_slider alias="home_slider"]');
+	get_template_part('inc/funnels-home');
+	get_template_part('inc/site-wide-cta');
+	get_footer(); 
+?>
