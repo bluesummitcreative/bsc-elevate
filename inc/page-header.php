e4af84f9e0f32eee4fb5b3a0353bd14aaa06030c
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-11-09 14:55:00 -0700

    Update page-header.php

--- inc/page-header.php
+++ inc/page-header.php
@@ -1,22 +1,24 @@
 <?php if ( is_home() ) { ?>
-<div class="container-fluid page-header" style="background:url(<?php if ( get_field( 'image', get_option('page_for_posts') ) ): the_field('image', get_option('page_for_posts')); else: the_field('default_secondary_page_header', 'option'); endif;?>) no-repeat;background-size:cover;background-position:center center;">
+<div class="container-fluid page-header" <?php if ( get_field( 'page_header_image',get_option('page_for_posts') ) ): ?>style="background:url( <?php the_field('page_header_image',get_option('page_for_posts') ); ?>) no-repeat;background-size:cover;background-position:center center;"<?php endif; ?>>
 <?php } else { ?>
-<div class="container-fluid page-header" style="background:url(<?php if ( get_field( 'page_header_image' ) ): the_field('page_header_image'); else: the_field('default_secondary_page_header', 'option'); endif;?>) no-repeat;background-size:cover;background-position:center center;">
+<div class="container-fluid page-header" <?php if ( get_field( 'page_header_image' ) ): ?>style="background:url( <?php the_field('page_header_image'); ?>) no-repeat;background-size:cover;background-position:center center;"<?php endif; ?>>
 <?php } ?>
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 text-center">
 				<h1><?php
 					if ( is_home() ) { 
-                            echo 'Blog';
-                        /*} elseif ( is_404() ) { 
-							echo '404 Not Found';*/
-						} elseif ( is_search() ) { 
-							echo $wp_query->found_posts . ' results found for "' . esc_html( get_search_query( false ) ) .'"';
-						
-						} else {
-							the_title();
-						}
+						echo 'Blog';
+					/*} elseif ( is_404() ) { 
+						echo '404 Not Found';*/
+					} elseif ( is_post_type_archive() ) {
+						echo post_type_archive_title();
+					} elseif ( is_search() ) { 
+						echo $wp_query->found_posts . ' results found for "' . esc_html( get_search_query( false ) ) .'"';
+
+					} else {
+						the_title();
+					}
 				?></h1>
 			</div>
 		</div>
