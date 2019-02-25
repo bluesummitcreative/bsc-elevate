f0c65f2798611adddae88ed8db1890b10d2d8a90
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-02-25 13:37:52 -0700

    Update loop-blog.php

--- inc/loop-blog.php
+++ inc/loop-blog.php
@@ -1,4 +1,4 @@
-<div class="col-sm-9 order-sm-2 content">
+<div class="col-sm-9 order-sm-2">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="row post">
 		<?php if(has_post_thumbnail()) { ?>
