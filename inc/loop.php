fb14c2bf595e8ba2e0035abecbc7186c2b02ffa2
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-02-25 13:37:45 -0700

    Update loop.php

--- inc/loop.php
+++ inc/loop.php
@@ -1,9 +1,9 @@
 <?php if ( is_page_template('full-width.php') ) { ?>
-<div class="col-sm-12 content">
+<div class="col-sm-12">
 <?php } elseif ( is_front_page() ) { ?>
-<div class="col-sm-12 content">
+<div class="col-sm-12">
 <?php } else { ?>
-<div class="col-sm-9 order-sm-2 content">
+<div class="col-sm-9 order-sm-2">
 <?php } ?>
 <?php if(has_post_thumbnail()) { ?>
 	<div class="row">
