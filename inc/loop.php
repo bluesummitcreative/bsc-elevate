54e5dd2d4cdde9ea286f08bd7ff875149a4b52b1
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2019-03-13 12:54:58 -0600

    Update loop.php

--- inc/loop.php
+++ inc/loop.php
@@ -2,24 +2,22 @@
 	<div class="container">
 		<div class="row">
 			
-		<?php if ( is_page_template('full-width.php') ) { ?>
-		<div class="col-sm-12">
-		<?php } elseif ( is_front_page() ) { ?>
-		<div class="col-sm-12">
-		<?php } else { ?>
-		<div class="col-sm-9 order-sm-2">
-		<?php } ?>
-		<?php if(has_post_thumbnail()) { ?>
-			<div class="row">
-				<div class="col-sm-12">
-					<?php the_post_thumbnail('medium_large', ['class' => 'img-responsive']);?>
-				</div>
-			</div>
-		<?php } else {} ?>
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<?php the_content(); ?>
-		<?php endwhile; endif; ?>
-		</div>
+			<?php 
+				if ( is_page_template('full-width.php') || is_front_page() ) {
+					echo '<div class="col-sm-12">';
+				} else {
+					echo '<div class="col-sm-9 order-sm-2">';
+				}
+
+					if ( have_posts() ) : while ( have_posts() ) : the_post();
+						the_content();
+					endwhile; endif; 
+
+					if (! is_page_template('full-width.php') || is_front_page() ) {
+						get_sidebar();
+					}
+				echo '</div>';
+			?>
 
 		</div>
 	</div>
